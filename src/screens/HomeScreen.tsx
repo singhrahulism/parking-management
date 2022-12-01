de06b28ee3cf9f939fadbf2795cde2191c7eb992
Rahul <rsinghbuddy@gmail.com>
2022-12-01 17:42:51 +0530

    initial commit

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+
+class HomeScreen extends Component {
+    render() {
+        return <View style={styles.container}>
+            <Text>This is HomeScreen</Text>
+        </View>
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        
+    }
+})
+
+export default HomeScreen ;
